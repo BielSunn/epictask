a1afda83c498493a04489c0cd372088bc40ff48d
Gabriel Sun <bielsun23@gmail.com>
2022-09-13 19:50:52 -0300

    first commit

---
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/UserController.java
@@ -0,0 +1,99 @@
+package br.com.fiap.epictaskapi.controller;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.epictaskapi.DTO.UserDto;
+import br.com.fiap.epictaskapi.model.User;
+import br.com.fiap.epictaskapi.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @GetMapping
+    //@Cacheable("users")
+    public Page<Object> index(@PageableDefault(size = 5, sort = "name") Pageable paginacao) {
+        var users = service.listAll(paginacao);
+        return users.map(user -> {
+            UserDto userDto = new UserDto();
+            userDto.setId(user.getId());
+            userDto.setName(user.getName());
+            userDto.setEmail(user.getEmail());
+            return userDto;
+        });
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody @Valid User user) {
+        //User usuario = user;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        service.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> show(@PathVariable Long id) {
+        return ResponseEntity.of(service.get(id));
+    }
+
+    @DeleteMapping("{id}")
+    //@CacheEvict(value = "tasks", allEntries = true)
+    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+        Optional<User> optional = service.get(id);
+        System.out.println("AQUI************************************");
+
+        if (optional.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        service.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<UserDto> update(@PathVariable Long id, @RequestBody @Valid UserDto newUser) {
+        // carregar a tarefa do banco
+        Optional<User> optional = service.get(id);
+        
+        // verificar se existe a tarefa com esse id
+        if (optional.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        // atualizar os dados
+        User user = optional.get();
+        BeanUtils.copyProperties(newUser, user);
+        user.setId(id);
+
+        // salvar a tarefa
+        service.save(user);
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setEmail(user.getEmail());
+        userDto.setName(user.getName());
+        return ResponseEntity.ok(userDto);
+    }
+}
---
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/model/User.java
@@ -0,0 +1,105 @@
+package br.com.fiap.epictaskapi.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.*;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "TB_USER")
+@SequenceGenerator(name = "use", sequenceName = "SQ_USER ", allocationSize = 1)
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(generator = "use", strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
